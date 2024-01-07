d39c620c67955599e87fa2e1da3bdf2b8097c163
John D <johnd@Johns-MBP.attlocal.net>
2024-01-07 14:27:08 -0600

    pseudo code after research

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,6 +20,17 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
+//first I need to write a function that includes day.js 
+//to display and update the time at the top of the page.
+//I then need to write a fucntion that differitiates the timeblocks
+//according to when the user is actually usuing the app vs past or present.
+
+
+
+
+
+$(function () {
+
 });
- new stuff
- 
--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 # Work Day Scheduler Starter Code
-=======
-# DayJsCalender
-new repo for calender  app
->>>>>>> refs/remotes/origin/main
